0f32730ffd1cf4802fe869fc6518b3709072b542
Joshua Nelson <jyn514@gmail.com>
2020-01-16 18:56:38 -0500

    Fix merge conflicts git didn't catch

--- src/data/error.rs
+++ src/data/error.rs
@@ -137,6 +137,9 @@ pub enum CppError {
     #[error("{0} is never terminated")]
     UnterminatedDirective(&'static str),
 
+    #[error("expected expression for #if")]
+    EmptyExpression,
+
     #[doc(hidden)]
     #[error("internal error: do not construct nonexhaustive variants")]
     __Nonexhaustive,
