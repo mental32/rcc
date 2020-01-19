2ea1fe310ffe6f6813bb33ec6f67abb99c54d5cd
Joshua Nelson <jyn514@gmail.com>
2020-01-19 13:20:18 -0500

    don't send so many ((((

--- fuzz/hfuzz.rs
+++ fuzz/hfuzz.rs
@@ -2,10 +2,16 @@
 extern crate honggfuzz;
 extern crate rcc;
 
+use rcc::data::lex::{Keyword::*, Token};
+use rcc::Lexer;
+use rcc::Locatable;
+
+fn lex(s: &str) -> Lexer {
+    Lexer::new("<test-suite>", s.chars(), false)
+}
+
 fn is_exotic_keyword(s: &str) -> bool {
-    use rcc::data::lex::{Keyword::*, Token};
-    use rcc::Locatable;
-    let (first, _) = rcc::Lexer::new("<test-suite>", s.chars(), false).first_token();
+    let (first, _) = lex(s).first_token();
     let first = match first {
         Some(Locatable {
             data: Token::Keyword(k),
@@ -20,11 +26,31 @@ fn is_exotic_keyword(s: &str) -> bool {
     }
 }
 
+fn too_many_parens(s: &str, n: usize) -> bool {
+    let tokens = lex(s).filter(Result::is_ok);
+    let mut parens_in_a_row = 0;
+    for token in tokens {
+        if let Ok(Locatable {
+            data: Token::LeftParen,
+            ..
+        }) = token
+        {
+            parens_in_a_row += 1;
+            if parens_in_a_row == n {
+                return true;
+            }
+        } else {
+            parens_in_a_row = 0;
+        }
+    }
+    false
+}
+
 fn main() {
     loop {
         fuzz!(|s: &[u8]| {
             if let Ok(s) = std::str::from_utf8(s) {
-                if !is_exotic_keyword(s) {
+                if !is_exotic_keyword(s) && !too_many_parens(s, 800) {
                     rcc::compile(s, "<fuzz test>".into(), false, false, false);
                 }
             }
